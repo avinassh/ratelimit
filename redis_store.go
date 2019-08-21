857d4818c470b02031d7a015013baade55ab923b
avi <hi@avi.im>
2019-08-21 16:34:09 +0530

    initial commit

---
+++ redis_store.go
@@ -0,0 +1,73 @@
+package ratelimit
+
+import (
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+type RedigoStore struct {
+	pool   *redis.Pool
+	script *redis.Script
+}
+
+func newRedisPool(address string) *redis.Pool {
+	return &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial(
+				"tcp",
+				address,
+			)
+		},
+	}
+}
+
+func NewRedigoStore(pool *redis.Pool) RedigoStore {
+	// we will initialise with the script
+	conn := pool.Get()
+	defer conn.Close()
+
+	var script = redis.NewScript(1, getScript())
+	err := script.Load(conn)
+	if err != nil {
+		panic(err)
+	}
+	return RedigoStore{pool: pool, script: script}
+}
+
+func (s *RedigoStore) inc(key string, rate, windowSize, now int) (map[string]int, error) {
+	conn := s.pool.Get()
+	defer conn.Close()
+
+	r, err := redis.IntMap(s.script.Do(conn, key, rate, windowSize, now))
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func (s *RedigoStore) Inc(key string, rate, windowSize, now int) (StoreResponse, error) {
+	return buildStoreResponse(s.inc(key, rate, windowSize, now))
+}
+
+func buildStoreResponse(result map[string]int, err error) (StoreResponse, error) {
+	if err != nil {
+		return StoreResponse{}, err
+	}
+	response := StoreResponse{
+		Counter:    result["c"],
+		LastRefill: MillisToTime(int64(result["ts"])),
+	}
+	if result["s"] == 1 {
+		response.Allowed = true
+	}
+	return response, nil
+}
+
+func TimeMillis(t time.Time) int64 {
+	return t.UnixNano() / int64(time.Millisecond)
+}
+
+func MillisToTime(m int64) time.Time {
+	return time.Unix(0, m*int64(time.Millisecond))
+}
---
+++ token_bucket.go
@@ -0,0 +1,62 @@
+package ratelimit
+
+import (
+	"fmt"
+	"time"
+)
+
+var timeNow = time.Now
+
+type Bucket struct {
+	identifier string
+	rate       int
+	windowSize int
+	store      Store
+}
+
+type Status struct {
+	Allowed     bool
+	Remaining   int
+	NextRefresh time.Duration
+}
+
+type StoreResponse struct {
+	Allowed    bool
+	Counter    int
+	LastRefill time.Time
+}
+
+// The Redis algorithm
+// given a key like `user-post:avinassh`, rate
+// if the doesn't exist then allow, refill and allow
+// if key exists
+// 		- check if the current value less than limit, if less allow and increment
+//		- if the limit has already exceeded, then see if it can be refilled
+
+// NewTokenBucket returns a new rate limiter which uses the token bucket algorithm.
+func NewTokenBucket(identifier string, rate, windowSize int, store Store) Bucket {
+	return Bucket{identifier, rate, windowSize, store}
+}
+
+func (b *Bucket) Allow(key string) (bool, error) {
+	s, err := b.AllowWithStatus(key)
+	if err != nil {
+		return false, err
+	}
+	return s.Allowed, nil
+}
+
+func (b *Bucket) AllowWithStatus(key string) (Status, error) {
+	userKey := fmt.Sprintf("%s:%s", b.identifier, key)
+	res, err := b.store.Inc(userKey, b.rate, b.windowSize, int(TimeMillis(timeNow())))
+	if err != nil {
+		return Status{}, err
+	}
+	timeElapsed := timeNow().Sub(res.LastRefill)
+	s := Status{
+		Allowed:     res.Allowed,
+		Remaining:   b.rate - res.Counter,
+		NextRefresh: timeElapsed + (time.Duration(b.windowSize) * time.Millisecond),
+	}
+	return s, nil
+}
